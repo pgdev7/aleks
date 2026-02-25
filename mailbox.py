02544668e2578dc05933a68bc461a49d11b6db8e
Pranav <pgdev7@gmail.com>
2026-02-25 00:17:50 +0000

    w

--- mailbox.py
+++ mailbox.py
@@ -1,1 +1,11 @@
+import math
 
+x_1 = 0.2
+x_2 = 0.3
+x_3 = 0.45
+z = 1938
+
+bottom_box = 2 * (x_1 * x_3) + x_1 * x_2 + 2 * (x_2 * x_3)
+top_box = 3.14 * (x_1/2) * (x_1/2) + 3.14 * (x_1/2) * x_2
+
+print(math.ceil((bottom_box + top_box) * z))
